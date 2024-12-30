03f47827b43c4bdb20cffabdbc7cd28f62cf23b9
unknown <ismail.ouali@ump.ac.ma>
2024-12-30 16:07:37 +0100

    EM-3 Configuring the routage

--- EmployeeManagement/src/app/app.routes.ts
+++ EmployeeManagement/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'employees', loadChildren: () => import('./employees/employees.module').then(m => m.EmployeesModule) },
+];
